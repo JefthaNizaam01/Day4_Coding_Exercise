10781cd375d2ec671d399ebe2dfe6938740e102c
Name <nizaam01.jeftha@gmail.com>
2024-05-17 14:09:25 +0200

    Initial_Commit

---
+++ script/code.js
@@ -0,0 +1,18 @@
+let person1 = {
+    name: "Nizaam",
+    surname: "Jeftha",
+    age: 23,
+   
+};
+
+let person2 = {
+    name: "Kiefah",
+    surname: "Palekar",
+    age: 20,
+};
+
+console.log(person1);
+
+let people = [person1, person2];
+
+console.log(people);
